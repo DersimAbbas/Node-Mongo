eea2b70112a31944ccd2f67aa2160f290f825260
DersimCloudHT24 <dersim.abbas@iths.se>
2025-01-20 11:48:10 +0100

    add connection to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+mongoose.
+connect("mongodb+srv://dersimcloud24:Delo12321@node-api-mongodb.eozli.mongodb.net/Products-API?retryWrites=true&w=majority&appName=node-api-mongodb")
+.then(() => {
+  console.log("connected to mongodb");
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
+
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/', (req, res) => { 
+  res.send('mongoAPI')
 })
-   
+  
+
+app.get('/home',(req, res) => {
+  res.sendFile(__dirname + "/api.html");
+})
+
+app.get('/C/user/:name', (req, res) => {
+  res.send("you are using ur url");
+});
+
+app.post('/blogpost', (req,res) => {
+  res.send("this is a post request");
+});
+
